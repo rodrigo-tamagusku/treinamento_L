c4e66600e53b50ee8c9554d028c7dbbf40405e17
rodrigo-tamagusku <rodrigo.tamagusku@gmail.com>
2021-04-16 17:02:16 -0300

    update atividade 5 sem herança direta

--- Atividades/Atividade 5/ListaPOO.cpp
+++ Atividades/Atividade 5/ListaPOO.cpp
@@ -2,10 +2,10 @@
 //rodrigo.tamagusku@gmail.com
 
 //Objetivo: Orientação a objetos: implementar as estruturas de dados anteriores com coneitos de OO: herança, sobrecarga de métodos, sobrecarga de operadores, polimorfismo
-//Herança: Check (Pilha é Lista e Fila é Lista).
-//Sobrecarga de Métodos: Check (Construtor de Node). No insertFirst usa o construtor sem nada. No insertLast usa o construtor que leva int.
+//Herança: Feito em outro arquivo (mas Pilha é Lista e Fila é Lista).
+//Sobrecarga de Métodos: Check (Construtor de Node). 
 //Sobrecarga de Operadores: Check (Fila-- e Pilha--)
-//Polimorfismo: Check (Função display, diferente pra se for lista, pilha ou fila também com insertFirst na pilha e insertLast na Fila)
+//Polimorfismo: Check (Função display, diferente pra se for lista, pilha ou fila)
 #include<iostream>
 #include <algorithm>
 #include<time.h>
@@ -31,53 +31,49 @@ class Node {
   	 void setNext(Node* N);
 };
 
-class ListaEncadeada{
-	public:
-		//int tamanho;
-		void display();					//public
-		int tamanho();					//public
-		ListaEncadeada();				//public
-		void removeFirst();				//public
-		Node* getFirst();				//public
-		void setFirst(Node* N);			//public
-		//void rotinaTeste();				//public
+class Lista{
+	public:	
+		Lista();
+		void TesteHardCode();
+		int tamanho();
+		void display();
 		void insertFirst(int);
-	private:
-		Node *inicio;
-		Node *fim;
-};
-
-class Lista:public ListaEncadeada{
-	public:
-		virtual void setLast(Node* N);	//private para pilha
-		virtual Node* getLast();		//private para pilha
-		void rotinaTeste();
-	private:
-		Node *inicio;
-		Node *fim;
-		void insertFirst(int val);
 		void insertLast(int val);
-		void removeLast();
+		void removeFirst();
+		void removeLast();			
+		Node* getLast();
+		void setLast(Node* N);	
+		Node* getFirst();
+		void setFirst(Node* N);
+	private:	
+		Node *fim;
+		Node *inicio;
 };
 
-class Pilha: public ListaEncadeada{
+class Pilha{
 	private:
-		//Lista *L = new Lista();	//
+		Lista *P;	//instancio uma Lista dentro da minha Pilha. Usarei ela e suas funções.
 	public:
-		Node* getLast();
-		void setLast(Node*);
-		void insertFirst(int); 	//polimorfirmo
+		void TesteHardCode();
+		void push(int); 
+		void pop();
 		void display();			//polimorfismo
 		Pilha();
 		void operator--();		//sobrecarga operador
 };
 
-class Fila: public Lista{
+class Fila{
+	private:	
+		Lista *F;	//instancio uma Lista dentro da minha Fila. Usarei ela e suas funções.
 	public:
+		void TesteHardCode();
 		void display(); 		//polimorfismo
 		Fila();
 		void operator--();		//sobrecarga operador
-		void insertLast(int);	//polimorfismo
+		void remove();
+		void insert(int);	
+		Node* getLast();
+		void setLast(Node* N);	
 };
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -107,32 +103,32 @@ void Node::setNext(Node* N){
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-//////////////Funções de Lista Encadeada
+//////////////Funções de Lista
 ////////////////////////////////////////////////////////////////////////////////////
 
-void ListaEncadeada::display() {		//equiavelente void diplay(Lista *L){
+void Lista::display() {		//equivalente void diplay(Lista *L){
    Node* ptr;
    if(inicio==NULL)
-   cout<<"List is empty";
+   		cout<<"List is empty";
    else {
       ptr = inicio;
-      cout<<"List elements are: ";
-      while (ptr != fim->getNext()) {
+      cout<<"List: ";
+      while (ptr!=NULL) {
          cout<< ptr->getData() <<" ";
          ptr = ptr->getNext();
       }
    }
-   cout<<endl;
+   cout << endl;
 }
 
-int ListaEncadeada::tamanho() {
+int Lista::tamanho() {
    struct Node* ptr;
    int contador=0;
    if(inicio==NULL)
    	  return 0;
    else {
       ptr = inicio;
-      while (ptr != fim->getNext()) {
+      while (ptr != NULL) {
          contador++;
          ptr = ptr->getNext();
       }
@@ -140,19 +136,11 @@ int ListaEncadeada::tamanho() {
    return contador;
 }
 
-ListaEncadeada::ListaEncadeada(){
-	setFirst(NULL);
-	//setLast(NULL);
-}
-
-void ListaEncadeada::removeFirst() {
+void Lista::removeFirst() {
    if(inicio==NULL)
    cout<<"Empty"<<endl;
    else {
-      //cout<<"The removed element is "<< inicio->getData() <<endl;
-      if(inicio->getNext()==NULL){ //1 elemento só
-      	fim=NULL;
-	  }
+      //cout<<"The removed element is "<< inicio->getData() <<endl
       Node* ptr=inicio;
       inicio = ptr->getNext();
       free(ptr);
@@ -160,157 +148,31 @@ void ListaEncadeada::removeFirst() {
    }
 }
 
-Node* ListaEncadeada::getFirst(){
+Node* Lista::getFirst(){
 	return inicio;
 }
-void ListaEncadeada::setFirst(Node* N){
+void Lista::setFirst(Node* N){
 	inicio=N;
 }
 
-void ListaEncadeada::insertFirst(int val) {
+Lista::Lista(){
+	setFirst(NULL);
+	setLast(NULL);
+}
+
+void Lista::insertFirst(int val) {
    //Node* newnode = (Node*) malloc(sizeof(Node));
    Node* newnode = new Node();
-   if(fim==NULL){ //se vazio
-   	  fim=newnode;
-   }
-   newnode->setData(val);
-   newnode->setNext(inicio);
-   inicio = newnode;
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//////////////Lista
-////////////////////////////////////////////////////////////////////////////////////
-
-void Lista::insertLast(int val) {
-   //Node* newnode = (Node*) malloc(sizeof(Node));
-   Node* newnode = new Node(val);
-   Node* ptr=fim;
-   if(inicio==NULL){ //se vazio
-   	  inicio=newnode;
-   }
-   else 
-   	  ptr->setNext(newnode);
-   //newnode->setData(val);
-   newnode->setNext(NULL);
-   fim=newnode;
-}
-Node* Lista::getLast(){
-	return fim;
-}
-void Lista::setLast(Node* N){
-	fim = N;
-}
-void Lista::removeLast() {
-   Node* ptr=inicio;
-   if(ptr==NULL)
-   cout<<"Empty"<<endl;
-   else {
-   	  while(ptr->getNext()!=fim){ //chego no penúltimo
-   	  	 ptr=ptr->getNext();
-   	  }
-      cout<<"The removed element is "<< ptr->getNext()->getData() <<endl;
-      if(inicio->getNext()==NULL){ //1 elemento só
-      	inicio=NULL;
-	  }
-	  free(fim);
-	  fim=ptr;
-	}
-}
-void Lista::rotinaTeste(){
-	display();
-	insertFirst(1);	display();
-	insertFirst(2);	display();
-	insertFirst(3);	display();
-	insertFirst(4);	display();
-	removeFirst(); 	display();
-	insertLast(5); 	display();
-	insertLast(6); 	display();
-	insertLast(7); 	display();
-	insertLast(8);	display();
-	insertLast(9);	display();
-	cout << endl;
-}
-////////////////////////////////////////////////////////////////////////////////////
-//////////////Pilha
-////////////////////////////////////////////////////////////////////////////////////
-
-void Pilha::setLast(Node *N){
-	//permissão negada
-}
-Node* Pilha::getLast(){
-	//permissão negada
-}
-
-void Pilha::insertFirst(int val){
-   Node* newnode = new Node();
-   /*		//não é necessário na pilha
    if(getLast()==NULL){ //se vazio
    	  setLast(newnode);
    }
-   */
    newnode->setData(val);
    newnode->setNext(getFirst());
    setFirst(newnode);
 }
 
-void Pilha::display(){
-   Pilha* aux = new Pilha;	//Pilha auxiliar para armazenar a Pilha principal. Ordem de O(n). Para manter a inserção e remoção na frente
-   //Node* ptr;
-   if(getFirst()==NULL)
-   	  cout<<"Stack is empty";
-   else {
-      //ptr = getFirst();
-      cout<<"Stack from top to bottom: ";
-      while (getFirst()!=NULL) {
-         cout<< getFirst()->getData() <<" ";
-         aux->insertFirst((getFirst())->getData());
-		 removeFirst();
-      }
-      while (aux->getFirst()!=NULL) {
-         insertFirst((aux->getFirst())->getData());
-		 aux->removeFirst();
-      }
-   }
-   delete aux;
-   cout<<endl;
-   /*
-   //Nota: Quebra regra de pilha. Deveria criar uma pilha temporária para fazer leitura e reconstrução.
-   struct Node* ptr;
-   if(getFirst()==NULL)
-      cout<<"Stack is empty";
-   else {
-      ptr = getFirst();
-      cout<<"Stack from top to bottom: ";
-      while (ptr != getLast()->getNext()) {
-         cout<< ptr->getData() <<" ";
-         ptr = ptr->getNext();
-      }
-   }
-   cout<<endl;
-   */
-}
-
-Pilha::Pilha(){
-	setFirst(NULL);
-	//setLast(NULL);
-}
-
-void Pilha::operator--(){
-	removeFirst();
-}
-
-////////////////////////////////////////////////////////////////////////////////////
-//////////////Fila
-////////////////////////////////////////////////////////////////////////////////////
-/*
-Node* Fila::getLast(){
-	return fim; 	//Dá problema, pois fim é private na classe Lista.
-}
-*/
-
-void Fila::insertLast(int val){
-	//Node* newnode = (Node*) malloc(sizeof(Node));
+void Lista::insertLast(int val) {
+   //Node* newnode = (Node*) malloc(sizeof(Node));
    Node* newnode = new Node(val);
    Node* ptr=getLast();
    if(getFirst()==NULL){ //se vazio
@@ -323,50 +185,117 @@ void Fila::insertLast(int val){
    setLast(newnode);
 }
 
+void Lista::removeLast() {
+   Node* ptr=getFirst();
+   if(ptr==NULL){
+   		cout<<"Empty"<<endl;
+   		return;
+   }
+   if(getFirst()->getNext()==NULL){ //1 elemento só
+      	free(getFirst());
+		setFirst(NULL);
+      	setLast(NULL);
+      	return;
+	}
+   else {
+   	  while(ptr->getNext()!=getLast()){ //chego no penúltimo
+   	  	 ptr=ptr->getNext();
+   	  }
+      //cout<<"The removed element is "<< ptr->getNext()->getData() <<endl;
+	  free(getLast());
+	  setLast(ptr);
+	  ptr->setNext(NULL);
+	}
+}
+void Lista::TesteHardCode(){
+	display();
+	cout<<"   Insere no primeiro: "; insertFirst(1);	display();
+	cout<<"   Insere no primeiro: "; insertFirst(2);	display();
+	cout<<"   Insere no primeiro: "; insertFirst(3);	display();
+	cout<<"   Insere no primeiro: "; insertFirst(4);	display();
+	cout<<"   Remove o primeiro: "; removeFirst(); 	display();
+	cout<<"   Remove o ultimo: "; removeLast(); 	display();
+	cout<<"   Insere no ultimo: "; insertLast(5); 	display();
+	cout<<"   Insere no ultimo: ";insertLast(6); 	display();
+	cout<<"   Insere no ultimo: ";insertLast(7); 	display();
+	cout<<"   Insere no ultimo: ";insertLast(8);	display();
+	cout<<"   Insere no ultimo: ";insertLast(9);	display();
+	cout<<"   Remove o ultimo: ";removeLast(); 	display();
+	cout << endl;
+}
+Node* Lista::getLast(){
+	return fim;
+}
+void Lista::setLast(Node* N){
+	fim = N;
+}
+////////////////////////////////////////////////////////////////////////////////////
+//////////////Pilha
+////////////////////////////////////////////////////////////////////////////////////
+
+void Pilha::TesteHardCode(){
+	display();
+	push(1);	display();
+	push(2);	display();
+	push(3);	display();
+	push(4);	display();
+	pop(); 	display();
+	cout << endl;
+}
+
+void Pilha::push(int val) {
+   P->insertFirst(val);
+}
+
+void Pilha::pop(){
+	P->removeFirst();
+}
+void Pilha::display(){
+    P->display();
+}
+
+Pilha::Pilha(){
+	P=new Lista();
+}
+
+void Pilha::operator--(){
+	P->removeFirst();
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//////////////Fila
+////////////////////////////////////////////////////////////////////////////////////
+void Fila::TesteHardCode(){
+	display();
+	//insertFirst(1);	display(); //erro pois não existe inserção no início da fila
+	remove(); 	display();
+	insert(1); 	display();
+	remove(); 	display();
+	insert(2); 	display();
+	insert(3); 	display();
+	insert(4);	display();
+	insert(5);	display();
+	remove(); 	display();
+	cout << endl;
+}
+
+void Fila::insert(int val){
+	F->insertLast(val);
+}
+void Fila::remove(){
+	F->removeFirst();
+}
+
 void Fila::display(){
-   Fila* aux = new Fila;	//Fila auxiliar para armazenar a fila principal. Ordem de O(n). Para manter a inserção atrás e remoção na frente
-   //Node* ptr;
-   if(getFirst()==NULL)
-   	  cout<<"Queue is empty";
-   else {
-      //ptr = getFirst();
-      cout<<"Queue from first to last: ";
-      while (getFirst()!=NULL) {
-         cout<< getFirst()->getData() <<" ";
-         aux->insertLast((getFirst())->getData());
-		 removeFirst();
-      }
-      while (aux->getFirst()!=NULL) {
-         insertLast((aux->getFirst())->getData());
-		 aux->removeFirst();
-      }
-   }
-   delete aux;
-   cout<<endl;
-   /*
-   //Nota: Quebra regra de Fila. 
-   struct Node* ptr;
-   if(getFirst()==NULL)
-     cout<<"Queue is empty";
-   else {
-      ptr = getFirst();
-      cout<<"Queue from first to last: ";
-      while (ptr != getLast()->getNext()) {
-         cout<< ptr->getData() <<" ";
-         ptr = ptr->getNext();
-      }
-   }
-   cout<<endl;
-   */
+   F->display();
 }
 
 Fila::Fila(){
-	setFirst(NULL);
-	setLast(NULL);
+	F = new Lista();
 }
 
 void Fila::operator--(){
-	removeFirst();
+	F->removeFirst();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -376,27 +305,30 @@ void Fila::operator--(){
 int main() {
    //srand (time(NULL));
    //system("PAUSE");
-   int option = 0;
+   int option,digito = 0;
    Lista *L;
    Pilha *P;
    Fila *F;
    do{
 	   cout << "Escolha a estrutura de dados: " << endl;
-	   cout << "   1 - Criar Lista " << endl;
-	   cout << "   2 - Criar Pilha " << endl;
-	   cout << "   3 - Criar Fila " << endl;
+	   cout << "   1 - Lista automatica" << endl;
+	   cout << "   2 - Pilha automatica" << endl;
+	   cout << "   3 - Fila automatica" << endl;
+	   cout << "   4 - Lista manual" << endl;
+	   cout << "   5 - Pilha manual" << endl;
+	   cout << "   6 - Fila manual" << endl;
 	   cout << "   0 - Sair " << endl;
 	   cin >> option;
 	   system("CLS");
 	   switch(option){
 	   	   case 1:
 			  L= new Lista();		//Onde eu crio a minha Lista
-			  L->rotinaTeste();
+			  L->TesteHardCode();
 			  delete L;
 			  break;
 		   case 2:
 		   	  P= new Pilha();		//Onde eu crio a minha Pilha
-		   	  P->rotinaTeste();
+		   	  P->TesteHardCode();
 		   	  cout << "Aplicando operador --"<<endl;
 		   	  P->operator--();		//Há maneira de puxar como P--; ?
 		   	  P->display();
@@ -404,10 +336,112 @@ int main() {
 		   	  break;
 		   case 3:
 		   	  F= new Fila();		//Onde eu crio a minha Fila
-		   	  F->rotinaTeste();
+		   	  F->TesteHardCode();
 		   	  cout << "Aplicando operador --"<<endl;
 		   	  F->operator--();
 		   	  F->display();
+		   	  delete F;
+		   	  break;
+		   case 4:
+		   	  L= new Lista();
+		   	  while(option!=9 && option!=0){
+				  cout << "Escolha a estrutura de dados: " << endl;
+				  cout << "   1 - Inserir no inicio da Lista " << endl;
+				  cout << "   2 - Inserir no fim da Lista " << endl;
+				  cout << "   3 - Remover no inicio da Lista " << endl;
+				  cout << "   4 - Remover no fim da Lista " << endl;
+				  cout << "   9 - Voltar" << endl;
+				  cout << "   0 - Sair " << endl;
+				  cin >> option;
+				  system("CLS");
+				  switch(option){
+				  	case 1:
+				  		cout <<"Digite o numero: "<< endl;
+				  		cin >> digito;
+				  		L->insertFirst(digito);
+				  		L->display();
+				  		break;
+				  	case 2:
+				  		cout <<"Digite o numero: "<< endl;
+				  		cin >> digito;
+				  		L->insertLast(digito);
+				  		L->display();
+				  		break;
+					case 3:
+				  		L->removeFirst();
+				  		L->display();
+				  		break;
+					case 4:
+				  		L->removeLast();
+				  		L->display();
+				  		break;
+					case 9:
+				  		break;
+				  	default:
+				  		break;
+				  }
+				  cout << endl;
+			  }
+			  delete L;
+			  break;
+		   case 5:
+		   	  P= new Pilha();		//Onde eu crio a minha Pilha
+		   	  while(option!=9 && option!=0){
+				  cout << "Escolha a estrutura de dados: " << endl;
+				  cout << "   1 - Inserir no topo da Pilha " << endl;
+				  cout << "   2 - Remover no topo da Pilha " << endl;
+				  cout << "   9 - Voltar" << endl;
+				  cout << "   0 - Sair " << endl;
+				  cin >> option;
+				  system("CLS");
+				  switch(option){
+				  	case 1:
+				  		cout <<"Digite o numero: "<< endl;
+				  		cin >> digito;
+				  		P->push(digito);
+				  		P->display();
+				  		break;
+				  	case 2:
+				  		P->pop();
+				  		P->display();
+				  		break;
+					case 9:
+				  		break;
+				  	default:
+				  		break;
+				  }
+				  cout << endl;
+			  }
+		   	  delete P;
+		   	  break;
+		   case 6:
+		   	  F= new Fila();		//Onde eu crio a minha Fila
+		   	  while(option!=9 && option!=0){
+				  cout << "Escolha a estrutura de dados: " << endl;
+				  cout << "   1 - Inserir no fim da Fila " << endl;
+				  cout << "   2 - Remover no inicio da Fila " << endl;
+				  cout << "   9 - Voltar" << endl;
+				  cout << "   0 - Sair " << endl;
+				  cin >> option;
+				  system("CLS");
+				  switch(option){
+				  	case 1:
+				  		cout <<"Digite o numero: "<< endl;
+				  		cin >> digito;
+				  		F->insert(digito);
+				  		F->display();
+				  		break;
+				  	case 2:
+				  		F->remove();
+				  		F->display();
+				  		break;
+					case 9:
+				  		break;
+				  	default:
+				  		break;
+				  }
+				  cout << endl;
+			  }
 		   	  delete F;
 		   	  break;
 		   default:
